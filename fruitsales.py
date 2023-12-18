bb9dc4bcb60a4e022798828d6bbd8fb53f622bda
Dev-Annie <85464723+Dev-Annie@users.noreply.github.com>
2023-12-17 20:16:16 -0500

    changed format

--- fruitsales.py
+++ fruitsales.py
@@ -1,4 +1,4 @@
 import pandas as pd
-fruit= pd.DataFrame([[35, 21], [41, 34]], columns=['Apples', 'Bananas'],
-                index=['2017 Sales', '2018 Sales'])
+fruit = pd.DataFrame({'Apples': [35,41], 'Bananas': [21,34]}, index = ['2017 Sales','2018 Sales'])
 fruit.to_csv("fruit.csv")
+print("Dataframe created and saved as 'fruit.csv'")
