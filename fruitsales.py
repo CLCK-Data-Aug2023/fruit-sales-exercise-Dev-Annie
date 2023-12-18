4e7ad1ed34b8c70dfaf73b92b2c91dca47013e9e
Dev-Annie <85464723+Dev-Annie@users.noreply.github.com>
2023-12-17 19:03:55 -0500

    csv added

--- fruitsales.py
+++ fruitsales.py
@@ -1,5 +1,5 @@
-# add your code install pandas as pd
+import pandas as pd
 fruit = pd.DataFrame([[30, 12]], columns=['Apples', 'Bananas'])
 fruit_sales = pd.DataFrame([[35, 21], [41, 34]], columns=['Apples', 'Bananas'],
                 index=['2017 Sales', '2018 Sales'])
-fruit.to_csv("fruit.csv")here
+fruit.to_csv("fruit.csv")
