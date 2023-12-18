2737137e09caf2170c7cd77f614bd29bce8ab9cb
Dev-Annie <85464723+Dev-Annie@users.noreply.github.com>
2023-12-17 19:12:15 -0500

    changed to install pandas

--- fruitsales.py
+++ fruitsales.py
@@ -1,4 +1,5 @@
-import pandas as pd
+#import pandas as pd
+install pandas as pd
 fruit = pd.DataFrame([[30, 12]], columns=['Apples', 'Bananas'])
 fruit_sales = pd.DataFrame([[35, 21], [41, 34]], columns=['Apples', 'Bananas'],
                 index=['2017 Sales', '2018 Sales'])
