73c9c44108dea49f40806525d32eb787ea76f1e4
Dev-Annie <85464723+Dev-Annie@users.noreply.github.com>
2023-12-17 19:21:45 -0500

    changed to sales

--- fruitsales.py
+++ fruitsales.py
@@ -1,5 +1,4 @@
 import pandas as pd
-fruit = pd.DataFrame([[30, 12]], columns=['Apples', 'Bananas'])
-fruit_sales = pd.DataFrame([[35, 21], [41, 34]], columns=['Apples', 'Bananas'],
+fruit= pd.DataFrame([[35, 21], [41, 34]], columns=['Apples', 'Bananas'],
                 index=['2017 Sales', '2018 Sales'])
 fruit.to_csv("fruit.csv")
